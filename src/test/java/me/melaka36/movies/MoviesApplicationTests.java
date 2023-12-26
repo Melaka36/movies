7d17f732eb7f0f0a4909e8e640b8eb8aaf5fb8d1
Melaka36 <melaka36pathiranagama@gmail.com>
2023-12-26 12:45:10 +0530

    initial commit

---
+++ src/test/java/me/melaka36/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.melaka36.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
