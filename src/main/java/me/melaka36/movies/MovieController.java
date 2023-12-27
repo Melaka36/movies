fb3a2299b476018c91e579ec3eee9eb74865a732
Melaka36 <melaka36pathiranagama@gmail.com>
2023-12-27 16:24:43 +0530

    findMovieByImdbId repository and service done

--- src/main/java/me/melaka36/movies/MovieController.java
+++ src/main/java/me/melaka36/movies/MovieController.java
@@ -19,8 +19,8 @@ public class MovieController {
     public ResponseEntity<List<Movie>> allMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
-        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id),HttpStatus.OK);
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
     }
 }
